4422d05a7236995badb5a2b6f009dbcbba042340
amandagpearce <amandagpearce@gmail.com>
2022-09-25 12:04:44 -0300

    setting up a not found page + programatic routing after new quote sent

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import QuoteForm from './../components/quotes/QuoteForm';
 
+import { useHistory } from 'react-router-dom';
+
 const NewQuote = () => {
+    const history = useHistory();
+
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData);
-    }
+
+        // if the data is sent, navigate to quotes list
+        history.push('/quotes')// pushing a new page to the stack of pages inside the user history
+        //history.push() allows to use the come back browser button, history.replace() does not since it replaces the current url, its more of a redirect
+     }
     
     return (
         <div>
